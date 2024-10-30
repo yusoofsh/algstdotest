162c9516456e42a87737602e06a81717b63bc62a
Yusoof Moh <me@yusoofsh.id>
2024-10-31 06:08:52 +0700

    build: target ios

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -12,83 +12,59 @@ import { Button } from "~/lib/components/button"
 import { Plus } from "~/lib/icons/plus"
 import { ChevronLeft } from "~/lib/icons/chevron"
 import { View } from "react-native"
-
-export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
-} from "expo-router"
-
-// Prevent the splash screen from auto-hiding before getting the color scheme.
-SplashScreen.preventAutoHideAsync()
+import { SafeAreaProvider } from "react-native-safe-area-context"
+export { ErrorBoundary } from "expo-router"
 
 export default function RootLayout() {
   const router = useRouter()
-  const { colorScheme, setColorScheme, isDarkColorScheme } = useColorScheme()
-  const [isColorSchemeLoaded, setIsColorSchemeLoaded] = useState(false)
-
-  useEffect(() => {
-    ;(async () => {
-      const theme = await AsyncStorage.getItem("theme")
-      if (!theme) {
-        AsyncStorage.setItem("theme", colorScheme)
-        setIsColorSchemeLoaded(true)
-        return
-      }
-      const colorTheme = theme === "dark" ? "dark" : "light"
-      if (colorTheme !== colorScheme) {
-        setColorScheme(colorTheme)
-        setIsColorSchemeLoaded(true)
-        return
-      }
-      setIsColorSchemeLoaded(true)
-    })().finally(() => {
-      SplashScreen.hideAsync()
-    })
-  }, [])
-
-  if (!isColorSchemeLoaded) {
-    return null
-  }
 
   return (
-    <ThemeProvider value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}>
-      <StatusBar style={isDarkColorScheme ? "light" : "dark"} />
-      <Stack
-        screenOptions={{
-          headerShadowVisible: false,
-          headerShown: true,
-        }}
-      >
-        <Stack.Screen
-          name="index"
-          options={{
-            headerTitle: "",
-            headerLeft: () => (
-              <Text className="text-xl font-bold">To Do List</Text>
-            ),
-            headerRight: () => (
-              <Link href="/new" asChild>
-                <Button variant="default" className="text-background flex-row">
-                  <Plus className="text-background" size={18} />
-                  <Text>New Task</Text>
-                </Button>
-              </Link>
-            ),
+    <SafeAreaProvider>
+      <ThemeProvider value={LIGHT_THEME}>
+        <StatusBar style="dark" />
+        <Stack
+          screenOptions={{
+            headerShadowVisible: false,
+            headerShown: true,
           }}
-        />
-        <Stack.Screen
-          name="new/index"
-          options={{
-            headerTitle: "",
-            headerLeft: () => (
-              <View className="flex-row gap-4" onTouchStart={() => router.back()}>
-                <ChevronLeft size={24} />
-                <Text className="text-xl font-bold">Add New Task</Text>
-              </View>
-            ),
-          }}
-        />
-      </Stack>
-    </ThemeProvider>
+        >
+          <Stack.Screen
+            name="index"
+            options={{
+              headerTitle: "",
+              headerLeft: () => (
+                <Text className="text-xl font-bold">To Do List</Text>
+              ),
+              headerRight: () => (
+                <Link href="/new" asChild>
+                  <Button
+                    variant="default"
+                    className="text-background flex-row"
+                  >
+                    <Plus className="text-background" size={18} />
+                    <Text>New Task</Text>
+                  </Button>
+                </Link>
+              ),
+            }}
+          />
+          <Stack.Screen
+            name="new/index"
+            options={{
+              headerTitle: "",
+              headerLeft: () => (
+                <View
+                  className="flex-row gap-4"
+                  onTouchStart={() => router.back()}
+                >
+                  <ChevronLeft size={24} />
+                  <Text className="text-xl font-bold">Add New Task</Text>
+                </View>
+              ),
+            }}
+          />
+        </Stack>
+      </ThemeProvider>
+    </SafeAreaProvider>
   )
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,116 @@
-import { View } from "react-native"
+import { SectionList, View } from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context"
 import { Button } from "~/lib/components/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/lib/components/card"
 import { Text } from "~/lib/components/text"
+
+const DATA = [
+  {
+    title: "Main dishes",
+    data: ["Pizza", "Burger", "Risotto"],
+  },
+  {
+    title: "Sides",
+    data: ["French Fries", "Onion Rings", "Fried Shrimps"],
+  },
+  {
+    title: "Drinks",
+    data: ["Water", "Coke", "Beer"],
+  },
+  {
+    title: "Desserts",
+    data: ["Cheese Cake", "Ice Cream"],
+  },
+  {
+    title: "Main dishes",
+    data: ["Pizza", "Burger", "Risotto"],
+  },
+  {
+    title: "Sides",
+    data: ["French Fries", "Onion Rings", "Fried Shrimps"],
+  },
+  {
+    title: "Drinks",
+    data: ["Water", "Coke", "Beer"],
+  },
+  {
+    title: "Desserts",
+    data: ["Cheese Cake", "Ice Cream"],
+  },
+  {
+    title: "Main dishes",
+    data: ["Pizza", "Burger", "Risotto"],
+  },
+  {
+    title: "Sides",
+    data: ["French Fries", "Onion Rings", "Fried Shrimps"],
+  },
+  {
+    title: "Drinks",
+    data: ["Water", "Coke", "Beer"],
+  },
+  {
+    title: "Desserts",
+    data: ["Cheese Cake", "Ice Cream"],
+  },
+  {
+    title: "Main dishes",
+    data: ["Pizza", "Burger", "Risotto"],
+  },
+  {
+    title: "Sides",
+    data: ["French Fries", "Onion Rings", "Fried Shrimps"],
+  },
+  {
+    title: "Drinks",
+    data: ["Water", "Coke", "Beer"],
+  },
+  {
+    title: "Desserts",
+    data: ["Cheese Cake", "Ice Cream"],
+  },
+  {
+    title: "Main dishes",
+    data: ["Pizza", "Burger", "Risotto"],
+  },
+  {
+    title: "Sides",
+    data: ["French Fries", "Onion Rings", "Fried Shrimps"],
+  },
+  {
+    title: "Drinks",
+    data: ["Water", "Coke", "Beer"],
+  },
+  {
+    title: "Desserts",
+    data: ["Cheese Cake", "Ice Cream"],
+  },
+]
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Button variant="outline" className="flex-row">
-        <Text>Go To Form</Text>
-      </Button>
-    </View>
+    <SafeAreaView>
+      <SectionList
+        sections={DATA}
+        stickySectionHeadersEnabled={true}
+        // contentContainerClassName="p-6 mx-auto w-full max-w-xl"
+        keyExtractor={(item, index) => item + index}
+        renderItem={({ item }) => (
+          <Card className="w-full max-w-sm">
+            <CardContent>
+              <Text>Card Content</Text>
+            </CardContent>
+          </Card>
+        )}
+        renderSectionHeader={({ section: { title } }) => <Text>{title}</Text>}
+      />
+    </SafeAreaView>
   )
 }
